122200cc9e71a192f6837fc77cfe44c3077fca3d
Oscar Melo <lilo921030@gmail.com>
2020-01-31 20:14:46 -0500

    culminacion del programa con tres planetas listo para mejoras

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-function mostrarPeso() {
-    if (masa == null) {
-        resultado.innerHTML = "Ingresa un valor"
-    }
-    else{
-        var masaUsuario = document.getElementById("masaUsuario")
-        masa = parseInt(masaUsuario.value)
-        resultado.innerHTML = "Este es tu peso " + masa
-        console.log(masa)
+
+function planetaElegido() {
+    var masa = document.getElementById("masaUsuario").value
+    var planeta = document.getElementById("planetaSeleccionado").value
+
+    if (masa == "") {
+        alert("Proporciona tu peso")
+    } else {
+            document.getElementById("masaUsuario").value = ""
+        }
+    
+    if (planeta == "") {
+        alert("Selecciona un planeta")
+    } else {
+    switch (planeta) {
+        case "Jupiter":
+            var masaFinal = masa * g_jupiter / g_tierra
+            break;
+        case "Marte":
+            var masaFinal = masa * g_marte / g_tierra
+            break;
+    
+        case "La Tierra":
+            var masaFinal = masa
+            break;
+        }
+        resultado.innerHTML = "Tu peso es " + masa + " kilogramos"
+        resultado2.innerHTML = "tu peso en " + planeta + " es " + masaFinal + " Kg" 
     }
 }
 
-var masa = null
-var planetaSeleccionado = document.getElementById("planetaSeleccionado")
+const g_tierra = 9.8
+const g_marte = 3.7
+const g_jupiter = 24.8
+
+var masaFinal
 var resultado = document.getElementById("resultado")
 var resultado2 = document.getElementById("resultado2")
 
 var enviar = document.getElementById("enviar")
-enviar.addEventListener("click", mostrarPeso)
+enviar.addEventListener("click", planetaElegido)
